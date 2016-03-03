5e8ee6b2ee4a186d9f09138af6f1640677dbf0e5
geere2046 <geere2046@163.com>
2016-03-03 15:39:41 +0800

    基本布局提交

--- app/src/main/java/com/yuzhi/fine/fragment/WildebeestFragment.java
+++ app/src/main/java/com/yuzhi/fine/fragment/WildebeestFragment.java
@@ -12,6 +12,7 @@ public class WildebeestFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_wildebeest, container, false);
     }
 
