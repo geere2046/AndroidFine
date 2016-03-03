57042901c2b320abad04d5b8f0cf6a51e48a7a9a
geere2046 <geere2046@163.com>
2016-03-03 16:12:09 +0800

    基本布局提交

--- app/src/main/java/com/yuzhi/fine/fragment/WildebeestFragment.java
+++ app/src/main/java/com/yuzhi/fine/fragment/WildebeestFragment.java
@@ -1,19 +1,99 @@
 package com.yuzhi.fine.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yuzhi.fine.R;
 
-public class WildebeestFragment extends Fragment {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class WildebeestFragment extends Fragment implements View.OnClickListener {
+
+    Context context;
+
+    @Bind(R.id.aaa)
+    TextView aaa;
+    @Bind(R.id.vw_title)
+    RelativeLayout vwTitle;
+    @Bind(R.id.tv_1)
+    TextView tv1;
+    @Bind(R.id.tv_2)
+    TextView tv2;
+    @Bind(R.id.tv_3)
+    TextView tv3;
+    @Bind(R.id.tv_4)
+    TextView tv4;
+    @Bind(R.id.tv_5)
+    TextView tv5;
+    @Bind(R.id.tv_6)
+    TextView tv6;
+    @Bind(R.id.tv_7)
+    TextView tv7;
+    @Bind(R.id.tv_8)
+    TextView tv8;
+    @Bind(R.id.tv_9)
+    TextView tv9;
+    @Bind(R.id.tv_10)
+    TextView tv10;
+    @Bind(R.id.vw_scroll)
+    ScrollView vwScroll;
+    @Bind(R.id.btn_finish)
+    Button btnFinish;
+    @Bind(R.id.btn_start)
+    Button btnStart;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_wildebeest, container, false);
+        View view = inflater.inflate(R.layout.fragment_wildebeest, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        context = getActivity();
+        initData();
+        initView();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    void initData() {
+
+    }
+
+    void initView() {
+        btnStart.setOnClickListener(this);
+        btnFinish.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start:
+                showToast("start");
+                break;
+            case R.id.btn_finish:
+                showToast("end");
+                break;
+        }
+    }
+
+    void showToast(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
 }
