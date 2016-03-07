53fe4ca04e69ce6fd2e0a60bc879df1ceea602f3
geere2046 <geere2046@163.com>
2016-03-07 18:14:40 +0800

    添加雷达图表

--- app/src/main/java/com/yuzhi/fine/fragment/WildebeestFragment.java
+++ app/src/main/java/com/yuzhi/fine/fragment/WildebeestFragment.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -14,18 +15,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.jxtii.wildebeest.model.PositionRecord;
-import com.jxtii.wildebeest.service.TaskService;
 import com.jxtii.wildebeest.util.CommUtil;
 import com.yuzhi.fine.R;
 
 import org.litepal.crud.DataSupport;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -39,16 +52,64 @@ public class WildebeestFragment extends Fragment implements View.OnClickListener
     static final int GPS_OPEN_STATUS = 1;
     Timer timer;
     TimerTask timerTask;
-    Handler handler = new Handler(){
+    Typeface tf;
+    String[] mParties = new String[]{
+            "急加速", "急减速", "急转弯", "疲劳", "超速", "路况"
+    };
+
+    Handler handler = new Handler() {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case 0:
-                    tv5.setText("急加速：" + (new Random().nextInt(100) % (100 - 10 + 1) + 10));
-                    tv6.setText("急减速：" + (new Random().nextInt(100) % (100 - 10 + 1) + 10));
-                    tv7.setText("急转弯：" + (new Random().nextInt(100) % (100 - 10 + 1) + 10));
-                    tv8.setText("疲劳：" + (new Random().nextInt(100) % (100 - 10 + 1) + 10));
-                    tv9.setText("超速：" + (new Random().nextInt(100) % (100 - 10 + 1) + 10));
-                    tv10.setText("路况：" + (new Random().nextInt(100) % (100 - 10 + 1) + 10));
+                    int v5 = new Random().nextInt(100) % (100 - 10 + 1) + 10;
+                    int v6 = new Random().nextInt(100) % (100 - 10 + 1) + 10;
+                    int v7 = new Random().nextInt(100) % (100 - 10 + 1) + 10;
+                    int v8 = new Random().nextInt(100) % (100 - 10 + 1) + 10;
+                    int v9 = new Random().nextInt(100) % (100 - 10 + 1) + 10;
+                    int v10 = new Random().nextInt(100) % (100 - 10 + 1) + 10;
+                    tv5.setText("急加速：" + v5);
+                    tv6.setText("急减速：" + v6);
+                    tv7.setText("急转弯：" + v7);
+                    tv8.setText("疲劳：" + v8);
+                    tv9.setText("超速：" + v9);
+                    tv10.setText("路况：" + v10);
+                    paraVw.setVisibility(View.GONE);
+
+                    int cnt = 6;
+                    ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+                    yVals1.add(new Entry(v5, 0));
+                    yVals1.add(new Entry(v6, 1));
+                    yVals1.add(new Entry(v7, 2));
+                    yVals1.add(new Entry(v8, 3));
+                    yVals1.add(new Entry(v9, 4));
+                    yVals1.add(new Entry(v10, 5));
+
+                    ArrayList<String> xVals = new ArrayList<String>();
+
+                    for (int i = 0; i < cnt; i++)
+                        xVals.add(mParties[i % mParties.length]);
+
+                    RadarDataSet set1 = new RadarDataSet(yVals1, "Set 1");
+                    set1.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+                    set1.setFillColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+                    set1.setDrawFilled(true);
+                    set1.setLineWidth(2f);
+
+                    ArrayList<IRadarDataSet> sets = new ArrayList<IRadarDataSet>();
+                    sets.add(set1);
+
+                    RadarData data = new RadarData(xVals, sets);
+                    data.setValueTypeface(tf);
+                    data.setValueTextSize(8f);
+                    data.setDrawValues(false);
+
+                    mChart.setData(data);
+                    for (IDataSet<?> set : mChart.getData().getDataSets())
+                        set.setDrawValues(!set.isDrawValuesEnabled());
+//                    mChart.animateY(1400);
+
+                    mChart.invalidate();
+
                     break;
             }
         }
@@ -84,6 +145,12 @@ public class WildebeestFragment extends Fragment implements View.OnClickListener
     Button btnFinish;
     @Bind(R.id.btn_start)
     Button btnStart;
+    @Bind(R.id.chart1)
+    RadarChart mChart;
+    @Bind(R.id.para_vw)
+    LinearLayout paraVw;
+    @Bind(R.id.bottom_vw)
+    LinearLayout bottomVw;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -113,6 +180,34 @@ public class WildebeestFragment extends Fragment implements View.OnClickListener
     void initView() {
         btnStart.setOnClickListener(this);
         btnFinish.setOnClickListener(this);
+
+        tf = Typeface.createFromAsset(getActivity().getAssets(), "OpenSans-Regular.ttf");
+
+        mChart.setDescription("你的数据");
+        mChart.setWebLineWidth(1.5f);
+        mChart.setWebLineWidthInner(0.75f);
+        mChart.setWebAlpha(100);
+
+        mChart.animateXY(
+                1400, 1400,
+                Easing.EasingOption.EaseInOutQuad,
+                Easing.EasingOption.EaseInOutQuad);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setTypeface(tf);
+        xAxis.setTextSize(9f);
+
+        YAxis yAxis = mChart.getYAxis();
+        yAxis.setTypeface(tf);
+        yAxis.setLabelCount(5, false);
+        yAxis.setTextSize(9f);
+        yAxis.setAxisMinValue(0f);
+
+        Legend l = mChart.getLegend();
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        l.setTypeface(tf);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
     }
 
     public void onClick(View v) {
@@ -190,14 +285,14 @@ public class WildebeestFragment extends Fragment implements View.OnClickListener
         getActivity().startService(ii);
     }
 
-    void startReflash(){
+    void startReflash() {
         timer = new Timer();
         timerTask = new TimerTask() {
             public void run() {
                 handler.sendEmptyMessage(0);
             }
         };
-        timer.scheduleAtFixedRate(timerTask, 10*1000, 5*1000);
+        timer.scheduleAtFixedRate(timerTask, 10 * 1000, 5 * 1000);
     }
 
     void stopReflash() {
